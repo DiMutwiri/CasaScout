552b9a7d07ef6051ce84e588f9cfb8681bf38573
DiMutwiri <125457677+DiMutwiri@users.noreply.github.com>
2023-12-28 21:20:04 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='text-red-500'>App
+     name = <input id="name" type="text" placeholder='Enter your name'/>
+    <h1 className='text-bold'> Hi there, I'm CasaScout</h1>
+    </div>
+   
+  )
+}
+
